09de27eddb5961fc67a2dc0c9405066fe1c9b944
flowmedia <flox2005@gmail.com>
2011-05-20 20:00:26 -0400

    some _unicode_ changes in models.py

--- polls/models.py
+++ polls/models.py
@@ -4,28 +4,17 @@ from django.db import models
 class Poll(models.Model):
 	question = models.CharField(max_length=200)
 	pub_date = models.DateTimeField()
+	def __unicode__(self):
+		return self.question
+
 
 class Choice(models.Model):
 	poll = models.ForeignKey(Poll)
 	choice = models.CharField(max_length=200)
 	votes = models.IntegerField()
-	
-class Poll(models.Model):
-	# ...
-	def __unicode__(self):
-		return self.question
-		
-class Choice(models.Model):
-	# ...
 	def __unicode__(self):
 		return self.choice
-		
-import datetime
-# ...
-class Poll(models.Model):
-	# ...
-	def was_published_today(self):
-		return self.pub_date.date() == datetime.date.today()
-		
+
+
 
 		
--- polls/views.py
+++ polls/views.py
@@ -1,1 +1,48 @@
 # Create your views here.
+from polls.models import Poll
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("Hello, world. You're at the poll index.")
+
+def detail(request, poll_id):
+	return HttpResponse("You're looking at poll %s." % poll_id)
+
+def results(request, poll_id):
+	return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+	return HttpResponse("You're voting on poll %s." % poll_id)
+	
+from polls.models import Poll
+from django.http import HttpResponse
+
+"""
+def index(request):
+	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+	output = ', '.join([p.question for p in latest_poll_list])
+	return HttpResponse(output)
+"""
+	
+from django.shortcuts import render_to_response
+
+def index(request):
+	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+	context = {'latest_poll_list': latest_poll_list}
+	return render_to_response('polls/index.html', context)
+	
+from django.http import Http404
+# ...
+def detail(request, poll_id):
+	try:
+		p = Poll.objects.get(id=poll_id)
+	except Poll.DoesNotExist:
+		raise Http404
+	return render_to_response('polls/detail.html', {'poll': p})
+
+from django.template import RequestContext
+# ...
+def detail(request, poll_id):
+	p = get_object_or_404(Poll, pk=poll_id)
+	return render_to_response('polls/detail.html', {'poll': p}, context_instance=RequestContext(request))
+
